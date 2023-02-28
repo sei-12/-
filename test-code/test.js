ab45eedda0b1f4bf8112b5b63df8851a1b1f99ec
おもちくん <sei.t11@icloud.com>
2023-03-01 06:59:06 +0900

    test loadBubble saveBubble

--- test-code/test.js
+++ test-code/test.js
@@ -29,11 +29,45 @@ const testBubbleCanBuild = function(){
     test(",aaa",false)
 }
 
+const testLoadAndSaveBubble = async function(){
+    con.log("Test : loadBubbles saveBubbles")
+    let config = {path:""}
+
+    const test = async function(fileStr){
+        count += 1
+        fileString = fileStr
+        if(await saveBubbles(config,await loadBubbles(config)) == fileStr){
+            con.log(`test${count} OK!`)
+        }else{
+            con.log(`test${count} NG!`)
+        }
+    }
+    let count = 0
+
+let test1 = 
+`aaa,red
+bbb,red
+ccc,red
+`
+    test(test1)
+    
+}
+
+window.myAPI = {
+    loadFile : async function(path){
+        return fileString
+    },
+    writeFile : async function(text,path){
+        console.log(text)
+    }
+}
+
 const testConsole = function(){
     for(let i = 0; i < 100; ++i){
         con.log(i)
     }
 }
-
+let fileString = ""
 const con = new Con()
 testBubbleCanBuild()
+testLoadAndSaveBubble()
